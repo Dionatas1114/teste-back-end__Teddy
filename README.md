1d7fefd7622fc4bd808d5b2031675863e03e8912
DESKTOP-H90VPRE\Diônatas Lazzari <sem.dionatas@gmail.com>
2021-05-28 19:23:40 -0300

    fix the public routes and readme file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # teste-back-end\_\_Teddy
 
 API as a test for the Teddy Open Banking
+
+The documentations is [here](https://documenter.getpostman.com/view/11754773/TzXzCcTY) and the postman collection is [here](https://www.getpostman.com/collections/e10d3c6b22cb8e84332c)
--- src/routes.js
+++ src/routes.js
@@ -14,17 +14,17 @@ const routes = Router();
 routes.use(cors());
 
 routes.get('/', (req, res) => {
-  res.json({ result: 'route test: API TEDDY OK' });
+  res.json({ result: 'test route: API TEDDY OK' });
 });
 
 //------------- PUBLIC ROUTES -------------//
 routes.post('/login', AuthController.store); //LOGIN
+routes.post('/users', userValidator, UserController.store); //* Atenção aos validators
 routes.put('/users/:uid', userValidator, UserController.update); //* Atenção aos validators
 
 //------------- PRIVATE ROUTES -------------//
-// USER ROUTES
 routes.use(AuthMiddleware);
-routes.post('/users', userValidator, UserController.store); //* Atenção aos validators
+// USER ROUTES
 routes.get('/users', UserController.index);
 routes.get('/users/:uid', UserController.show);
 
